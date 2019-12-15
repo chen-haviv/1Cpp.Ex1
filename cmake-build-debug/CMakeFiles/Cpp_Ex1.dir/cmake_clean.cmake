337fa44e31ee89528f2cece43c15a8ec6427a26c
chen.haviv <chen.haviv@mail.huji.ac.il>
2019-12-15 18:45:12 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Cpp_Ex1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Ex1.dir/main.cpp.o"
+  "Cpp_Ex1.exe"
+  "Cpp_Ex1.pdb"
+  "libCpp_Ex1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
