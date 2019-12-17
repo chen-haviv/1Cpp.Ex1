c28d760ecb8483c68ac23f860d0e23c4f143149f
chen.haviv <chen.haviv@mail.huji.ac.il>
2019-12-17 16:59:52 +0200

    Added the input operator to the class declaration.

--- Matrix.h
+++ Matrix.h
@@ -3,12 +3,11 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 #define ADDITION_ERROR_MSG "Incompatible rows and columns in addition"
-#define INCOMPATIBLE_FILE_TO_MATRIX "Number of numbers read does not match "\
-                                                     "expected matrix size."
+#define INCOMPATIBLE_FILE_TO_MATRIX "Number of numbers read does not match expected matrix size."
 #define INVALID_INPUT "Invalid input from input file."
 /**
  * @struct MatrixDims
- * @brief Matrix dimensions container
+ * @brief Matrix dimensions container todo what is this???
  */
 typedef struct MatrixDims
 {
@@ -30,9 +29,9 @@ public:
 	~Matrix();
 
 	// Methods:
-	const int& getRows();
+	const int &getRows();
 
-	const int& getCols();
+	const int &getCols();
 
 	void vectorize();
 
@@ -45,17 +44,18 @@ public:
 
 	Matrix operator*(const float &c);
 
-	friend Matrix operator*(const float &c, const Matrix& a);
+	friend Matrix operator*(const float &c, const Matrix &a);
 
 	Matrix operator+(const Matrix &b) const;
 
 	Matrix &operator+=(const Matrix &b);
 
-	const float& operator()(int i, int j) const;// todo - return value should be & or not?? consts?
-	float& operator()(int i, int j);// todo - return value should be & or not?? consts?
+	const float &operator()(int i, int j) const;// todo - return value should be & or not?? consts?
+	float &operator()(int i, int j);// todo - return value should be & or not?? consts?
 
-	float& operator[](int i);
-	const float& operator[](int i) const;
+	float &operator[](int i);
+
+	const float &operator[](int i) const;
 
 	friend std::ostream &operator<<(std::ostream &out, const Matrix &m);
 
@@ -64,7 +64,7 @@ public:
 private:
 	int rows;
 	int cols;
-	float*matrix; // pointer to the single dimensional array which is our matrix
+	float *matrix; // pointer to the single dimensional array which is our matrix
 
 
 
