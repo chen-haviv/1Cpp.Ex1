6f67fbd9c3bc7bdb2f0003ebb3df5317dbf03139
chen.haviv <chen.haviv@mail.huji.ac.il>
2019-12-18 10:14:16 +0200

    Fixed vectorize to return a reference

---
+++ cmake-build-debug/CMakeFiles/Cpp_Ex1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Ex1.dir/Activation.cpp.o"
+  "CMakeFiles/Cpp_Ex1.dir/Matrix.cpp.o"
+  "CMakeFiles/Cpp_Ex1.dir/main.cpp.o"
+  "CMakeFiles/Cpp_Ex1.dir/tmpmain.cpp.o"
+  "Cpp_Ex1.exe"
+  "Cpp_Ex1.pdb"
+  "libCpp_Ex1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
